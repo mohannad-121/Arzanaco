31b283e8477408e704b2082fad9c286e6b0695fb
mohannad-121 <mabuayyash33@gmail.com>
2026-08-16 23:21:21 +0300

    Improve quote checkboxes and responsive navigation

--- artifacts/arzana-website/src/components/ArzanaCheckbox.tsx
+++ artifacts/arzana-website/src/components/ArzanaCheckbox.tsx
@@ -42,9 +42,27 @@ export function ArzanaCheckbox({
         />
         <span className="arzana-checkbox-frame" aria-hidden="true">
           <span className="arzana-checkbox-box">
+            <span className="arzana-checkbox-bg" />
             <svg viewBox="0 0 24 24" className="arzana-checkbox-check">
               <path d="M3.25 12.5 9.5 18.75 20.75 5.5" />
             </svg>
+            <span className="arzana-checkbox-particle arzana-checkbox-particle--1" />
+            <span className="arzana-checkbox-particle arzana-checkbox-particle--2" />
+            <span className="arzana-checkbox-particle arzana-checkbox-particle--3" />
+            <span className="arzana-checkbox-particle arzana-checkbox-particle--4" />
+            <span className="arzana-checkbox-particle arzana-checkbox-particle--5" />
+            <span className="arzana-checkbox-particle arzana-checkbox-particle--6" />
+            <span className="arzana-checkbox-ring arzana-checkbox-ring--1" />
+            <span className="arzana-checkbox-ring arzana-checkbox-ring--2" />
+            <span className="arzana-checkbox-ring arzana-checkbox-ring--3" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--1" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--2" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--3" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--4" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--5" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--6" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--7" />
+            <span className="arzana-checkbox-spark arzana-checkbox-spark--8" />
           </span>
         </span>
       </span>
